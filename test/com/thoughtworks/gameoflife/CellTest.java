8b550caaecbf0330f82587a5b93c99fea6d175a1
thejasbabu <thejasb@thoughtworks.com>
2015-09-19 10:40:45 +0530

    Added a spec for dead cell to return Dead

--- test/com/thoughtworks/gameoflife/CellTest.java
+++ test/com/thoughtworks/gameoflife/CellTest.java
@@ -6,9 +6,16 @@ import static org.junit.Assert.*;
 public class CellTest {
 
     @Test
-    public void shouldReturnTheStateOfCell() {
+    public void shouldReturnTheStateOfCellAsAlive() {
         Cell cell = new Cell(1, 2, true);
 
         assertEquals("Alive", cell.stateOfCell());
+    }
+
+    @Test
+    public void shouldReturnTheStateOfTheCellAsDead() {
+        Cell cell = new Cell(1, 2, false);
+
+        assertEquals("Dead", cell.stateOfCell());
     }
 }
